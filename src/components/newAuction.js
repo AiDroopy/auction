b0461c13413bf1bb2c73d004a4e84fae23b5797a
zeenaamin <zeena_alamely@yahoo.com>
2022-03-24 15:46:04 +0100

    test

---
+++ src/components/newAuction.js
@@ -0,0 +1,90 @@
+import { useContext, useState } from "react";
+import AuctionContext from "../context/AuctionContext";
+
+
+const newAuction = () => {
+    const { user, createNew, isLoading, authUser} = useContext (AuctionContext);  
+    const [startPrice, setStartPrice] = useState("");
+    const [startDate, setStarDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [endTime, setEndTime] = useState("");
+    const [info, setInfo] = useState("");
+    const [img, setImg] = useState("");
+
+
+    
+    return ( <div className="new">
+    <h2>Creat Auction</h2>
+        <form>
+            <label htmlFor="title">Title: </label>
+            <input 
+                type="title"
+                required 
+                value={users.title}
+                onChange={handleOnChange}
+              
+            />
+            <label htmlFor="stratPrice">Start Price:</label>
+            <input 
+                type="Start Price"
+                required 
+                value={users.startPrice}
+                onChange={handleOnChange}
+                
+            />
+            
+            <label htmlFor="startDate">Start Date: </label>
+            <input 
+                Month = "set Month"
+                Day = "set Day"
+                required 
+                value={users.startDate}
+                onChange={handleOnChange}
+              
+            />
+
+            
+            <label htmlFor="endtDate">End Date: </label>
+            <input 
+                Month = "set Month"
+                Day = "set Day"
+                required 
+                value={users.endDate}
+                onChange={handleOnChange}
+              
+            />
+
+        
+            <label htmlFor="endTime">Start Time: </label>
+            <input 
+                Hours = "set Hour"
+                Minutes = "set Minute"
+                required 
+                value={users.endTime}
+                onChange={handleOnChange}
+              
+            />
+
+            
+            <label htmlFor="info">Information: </label>
+            <input 
+                type = "text"
+                required 
+                value={users.info}
+                onChange={handleOnChange}
+              
+            />
+
+
+
+
+
+
+            <button type="submit" onClick={handleSubmit}>
+                Add Auction
+            </button>
+        </form> 
+    </div>);
+}
+ 
+export default newAuction;
