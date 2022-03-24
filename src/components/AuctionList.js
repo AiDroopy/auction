4343e8344ffc93c913a54c6ecdeda652766cf066
Mmarill <Max.henkesson@gmail.com>
2022-03-24 13:23:15 +0100

    Show auctions

--- src/components/AuctionList.js
+++ src/components/AuctionList.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useContext } from 'react';
+import AuctionContext from '../context/AuctionContext';
+import Card from "./Card";
 
 const AuctionList = () => {
-  return (
-    <div>AuctionList</div>
-  )
-}
 
-export default AuctionList
+    const { auctions } = useContext(AuctionContext);
+    
+    return (
+
+        <div className="auctions"> 
+        {auctions.map((auction) => (<div className="auctions" key={auction.auctionId}> 
+        <br></br>
+        Product name: {auction.productName}
+        <br></br>
+        Description: {auction.productInfo}
+        <br></br>
+        <img src={auction.productImage}></img>
+        <br></br>
+        Starting price: {auction.Startprice}
+        <br></br>
+        Bids: {auction.Bids}
+        
+
+        </div>))} 
+         </div>
+
+      );
+    }
+
+export default AuctionList;
