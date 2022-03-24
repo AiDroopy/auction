b450b1db1d75a3f87c2257c93a3c961ceb989d4a
Mmarill <Max.henkesson@gmail.com>
2022-03-24 09:57:45 +0100

    fetchAuctions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './components/Home';
 import { AuctionProvider } from './context/AuctionContext';
 import Profile from './components/Profile';
 import Login from './components/Login';
+import LoginForm from './components/LoginForm';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
@@ -15,6 +17,11 @@ function App() {
       <div className="content">
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route path='/LoginForm' element={<LoginForm />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Profile' element={<Profile />} />
+          <Route path='/SignUp' element={<SignUp />} />
+          
         </Routes>
       </div>
       </AuctionProvider>  
