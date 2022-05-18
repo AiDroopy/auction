5bbf2caf21edd95fb8bb2498467974e8e538cf39
pirtyraven <89965279+pirtyraven@users.noreply.github.com>
2022-05-18 10:46:10 +0200

    Fixed renderhighbid
    Fixed renderhighbid-function to auction context so that we can use it in multple components/pages.
    
    Fixed that it's no longer possible to bid on a closed auction.

--- src/components/AuctionList.js
+++ src/components/AuctionList.js
@@ -1,33 +1,12 @@
 import React from 'react'
 import { useContext } from 'react';
 import AuctionContext from '../context/AuctionContext';
-import CountdownTimer from './CountdownTimer/CountdownTimer';
-import Bid from "./Bid";
 import { Link } from 'react-router-dom';
  
 
 const AuctionList = () => {
 
-    const { auctions, bids} = useContext(AuctionContext);
-
-    const renderHighBid = (theBids, auction) => {
-        let highBid = 0
-        theBids.map((bid) => {
-        if (bid.auctionId === auction.id){
-            if (highBid === 0 || highBid < bid.amount){
-                highBid = bid.amount
-            }
-        }})
-        if(Date.parse(auction.endTime) < Date.parse(Date())){
-            return <div> Auction closed with winning bid: {highBid} </div> 
-        }
-        if(Date.parse(auction.endTime) > Date.parse(Date())){
-            return <div>
-                 Highest bid: {highBid} 
-                 <CountdownTimer
-                    countdownTimestampMs={auction.endTime}/></div> 
-        }
-    }
+    const { auctions, bids, renderHighBid} = useContext(AuctionContext);
 
     
     return (
@@ -45,8 +24,6 @@ const AuctionList = () => {
                     
                 <br></br>
                     {renderHighBid(bids, auction)}
-            
-                {< Bid auctionId={ auction.id }/>}
     
         </div></Link>))} 
          </div>
--- src/components/Pages/SingleAuction.js
+++ src/components/Pages/SingleAuction.js
@@ -1,10 +1,14 @@
-import React, {useState, useEffect } from 'react'
+import React, {useState, useEffect, useContext } from 'react'
 import {useParams} from 'react-router-dom'
 import AuctionService from '../../services/AuctionService'
+import AuctionContext from '../../context/AuctionContext'
+import AuctionList from '../AuctionList'
 
 const SingleAuction = () => {
 
     const [auction, setAuction] = useState([])
+
+    const {bids, renderHighBid} = useContext(AuctionContext);
 
     const {id} = useParams();
 
@@ -13,7 +17,20 @@ const SingleAuction = () => {
         }, []);
     
     return (
-      <div>{auction.id}</div>
+      <div className="auctions" key={auction.id}> 
+                <br></br>
+                    Product name: {auction.productName}
+                <br></br>
+                    Description: {auction.productInfo}
+                <br></br>
+                    <img src={auction.productImgURL} alt="img" className="img"></img>
+                <br></br>
+                    Starting price: {auction.startPrice}
+                    
+                <br></br>
+                    {renderHighBid(bids, auction)}
+    
+        </div>
     )
 }
 
