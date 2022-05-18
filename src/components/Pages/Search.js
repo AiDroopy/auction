e82641312f230f3d94ab6eae3676d10ed377a967
pirtyraven <89965279+pirtyraven@users.noreply.github.com>
2022-05-18 10:21:00 +0200

    Fixed that Searched auctions are clickable

--- src/components/Pages/Search.js
+++ src/components/Pages/Search.js
@@ -1,5 +1,6 @@
 import AuctionContext from "../../context/AuctionContext"
 import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Search = () => {
 
@@ -9,15 +10,15 @@ const Search = () => {
   return (
     <div className="search-bar">
         <input type="text" placeholder="Search..." onChange={event => {setTheSearch(event.target.value)}}/>
-        {auctions.filter((val)=>{
-            if (theSearch == ''){
-                return (<div>{val.productName}</div>)
+        {auctions.filter((auction)=>{
+            if (theSearch === ''){
+                return (<div>{auction.productName}</div>)
                 }
-                else if(val.productName.toLowerCase().includes(theSearch.toLowerCase())){
-                    return(<div>{val.productName}</div>)
+                else if(auction.productName.toLowerCase().includes(theSearch.toLowerCase())){
+                    return(<div>{auction.productName}</div>)
                 }
-        }).map((val) => {
-            return (<div>{val.productName}</div>)
+        }).map((auction) => {
+            return (<Link to={`/auction/${auction.id}`}><div>{auction.productName}</div></Link>)
         })}
     </div>
 )}
