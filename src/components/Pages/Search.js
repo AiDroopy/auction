5eac548e20e88f83e837624668dea7eb4b111b74
zeenaamin <zeena_alamely@yahoo.com>
2022-05-18 08:12:05 +0200

    Update Search.js

--- src/components/Pages/Search.js
+++ src/components/Pages/Search.js
@@ -4,22 +4,27 @@ import React, { useContext, useState } from "react";
 const Search = () => {
 
     const { auctions } = useContext(AuctionContext);
-    const [theSearch, setTheSearch] = useState('')
+    const [theSearch, setTheSearch] = useState("");
+    
+    
 
   return (
     <div className="search-bar">
         <input type="text" placeholder="Search..." onChange={event => {setTheSearch(event.target.value)}}/>
         {auctions.filter((val)=>{
             if (theSearch == ''){
-                return (<div>{val.productName}</div>)
-                }
+                return val
+            }
                 else if(val.productName.toLowerCase().includes(theSearch.toLowerCase())){
-                    return(<div>{val.productName}</div>)
+                    return val.productName
                 }
-        }).map((val) => {
-            return (<div>{val.productName}</div>)
-        })}
-    </div>
-)}
+            }).map((val) => {
+                return (
+                    <div class="auctions"> {val.productName}
+                    </div>
+                );
+            })}
+        </div>
+    )}
 
 export default Search
