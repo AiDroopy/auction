2dd54825c7e31f4a70471f73bd34a7236f9c1441
Mmarill <Max.henkesson@gmail.com>
2022-05-19 21:01:27 +0200

    routes not caps

--- src/App.js
+++ src/App.js
@@ -29,15 +29,15 @@ function App() {
 
           <Route path='/' element={<Home />} />
 
-          <Route path='/DeliveryForm' element={<DeliveryForm />} />
+          <Route path='/deliveryForm' element={<DeliveryForm />} />
           
-          <Route path='/LoginForm' element={<LoginForm />} />
-          <Route path='/Destination' element={<DeliveryOptions/>} />
-          <Route path='/Profile' element={<Profile />} />
-          <Route path='/NewAuction' element={<NewAuction />} />
-          <Route path='/Search' element={<Search />} />
-          <Route path='/SignUpForm' element={<SignUpForm />} />
-          <Route path='/AuctionList' element={<AuctionList />} />
+          <Route path='/loginForm' element={<LoginForm />} />
+          <Route path='/destination' element={<DeliveryOptions/>} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/newAuction' element={<NewAuction />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/signUpForm' element={<SignUpForm />} />
+          <Route path='/auctionList' element={<AuctionList />} />
         </Routes>
       </div>
       </AuctionProvider> 
