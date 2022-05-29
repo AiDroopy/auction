8de0dcc7678a0b0afad561d0e8849ca69bdc1b43
zeenaamin <zeena_alamely@yahoo.com>
2022-05-29 03:11:56 +0200

    login test

--- cypress/integration/login.test.js
+++ cypress/integration/login.test.js
@@ -1,18 +1,18 @@
+
 describe('login', () =>{
 
     it('user login',()=>{
-        const username = 'eva@test.se'
-        const password = '123visit'
+
+        const email = 'dada@sese.se'
+        const password = 'visiting'
 
         cy.visit('http://localhost:3000')
-        cy.contains('a', "login").click
+        cy.contains('h2', 'Login').click()
         cy.get('form').submit()
-        cy.get('input[type="email"]').should('have.value', 'Username')
-        cy.get('input[type="password"]').should('have.value', 'Password')
-        
+       
+        cy.get('[name=username]').type(email)
+        cy.get('[name=password]').type(password)
 
-
-        
 
 
     })
