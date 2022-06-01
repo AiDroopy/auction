9dcbe08d8450ea8089f76dcbf4ea82a6ead6d8a5
Mmarill <Max.henkesson@gmail.com>
2022-06-02 00:40:19 +0200

    fix

--- src/components/Pages/Profile.js
+++ src/components/Pages/Profile.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import AuctionContext from "../../context/AuctionContext";
-import { Row, Container, Button, Form } from "react-bootstrap"; 
+import { Row, Container, Button, Form, ListGroup, Badge } from "react-bootstrap"; 
 import AuthService from "../../services/AuthService";
 import Auction from "../Card";
 import AuctionLink from "../CardLnk";
@@ -14,7 +14,6 @@ const Profile = () => {
   return (
           
       <>
-      <div key={currentUser.id}>
         <Form.Control className="profileForm"
           type="text"
           placeholder={currentUser.username}
@@ -24,46 +23,69 @@ const Profile = () => {
       
       <br></br>
       <div className="myAuctions">
+      <Container className='row-wrapper'>
           <h3>Your current auctions: </h3>
-              <Container>
-                    <div className='row-wrapper'>
                         <Row>
                         {auctions.map((auction => { 
                           if (auction.userId === currentUser.id) 
-                              return <AuctionLink key={auction.id} auction={auction}/>}))}
+                              return<Link className="wonauctions" to={`/auction/${auction.id}`}>
+                              <ListGroup as="ol" numbered>
+                                    <ListGroup.Item
+                                      as="li"
+                                      className="d-flex justify-content-between align-items-start"
+                                    >
+                                      <div className="ms-2 me-auto">
+                                        <div className="fw-bold">{auction.productName}</div>
+                                      {auction.productInfo}
+                                      </div>
+                                      <img className="imglst" src={auction.productImgURL}></img>
+
+                                    </ListGroup.Item></ListGroup>
+                                    </Link>}))}
+
                         </Row>
-                    </div>
-              </Container>    
+      </Container>    
+      <Container className='row-wrapper'>
+               
+          <Row>
+              <h3>Your won auctions: </h3>
+              <h5>Press auction to enter delivery details:</h5>
+                {auctions.map((auction => { 
+                  let highBid = 0
+                  let highBidUserId = ''
+                
+
+                  bids.forEach((bid) => {
+                      if (bid.auctionId === auction.id){
+                      if (highBid === 0 || highBid < bid.amount){
+                          highBid = bid.amount
+                          highBidUserId = bid.userId
+                        }}}
+                )
+
+              if (Date.parse(auction.endTime) < Date.parse(Date()) && highBidUserId === currentUser.id)  
+                      return <Link className="wonauctions" to={`/delivery/${auction.id}`}>
+                                <ListGroup as="ol" numbered>
+                                      <ListGroup.Item
+                                        as="li"
+                                        className="d-flex justify-content-between align-items-start"
+                                      >
+                                        <div className="ms-2 me-auto">
+                                          <div className="fw-bold">{auction.productName}</div>
+                                        {auction.productInfo}
+                                        </div>
+                                        <img className="imglst" src={auction.productImgURL}></img>
+
+                                      </ListGroup.Item></ListGroup>
+                                      </Link>}))}
+
+          </Row>
+      </Container>
           
-          <h3>Your won auctions: </h3>
-          <h5>Press auction to enter delivery details:</h5>
-              <Container>
-                    <div className='row-wrapper'>
-                        <Row>
-                        {auctions.map((auction => { 
-                          let highBid = 0
-                          let highBidUserId = ''
-                        
-
-                          bids.forEach((bid) => {
-                              if (bid.auctionId === auction.id){
-                              if (highBid === 0 || highBid < bid.amount){
-                                  highBid = bid.amount
-                                  highBidUserId = bid.userId
-                                }}}
-                        )
-
-                      if (Date.parse(auction.endTime) < Date.parse(Date()) && highBidUserId === currentUser.id)  
-                              return <Link className="wonauctions" to={`/delivery/${auction.id}`}><Auction key={auction.id} auction={auction}/></Link>}))}
-
-                        </Row>
-                    </div>
-              </Container>
-              <br></br>
-          <h3>Your ongoing bids: </h3>
-            <Container>
-                    <div className='row-wrapper'>
-                        <Row>
+      <Container className='row-wrapper'>
+           
+          <Row>
+                <h3>Your ongoing bids: </h3>
                         {auctions.map((auction => { 
                           let bidder = false
                             bids.forEach((bid) => {
@@ -72,12 +94,10 @@ const Profile = () => {
                           }})
                     if (bidder && Date.parse(auction.endTime) > Date.parse(Date()))
                       return <AuctionLink key={auction.id} auction={auction}/>}))}
-                </Row>
-              </div>
-            </Container>
+            </Row>
+          </Container>
           
-          </div>
-      </div>
+        </div>
       </>
       )
 }         
