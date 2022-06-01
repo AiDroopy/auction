e5bd9819f4028ed6b4b3c938b005029bca0cd4f7
Mmarill <Max.henkesson@gmail.com>
2022-06-01 21:13:49 +0200

    css

--- src/components/AuctionList.js
+++ src/components/AuctionList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useContext } from 'react';
 import AuctionContext from '../context/AuctionContext';
 import { Link } from 'react-router-dom';
-import { Card, CardGroup } from 'react-bootstrap';
+import { Card, Row } from 'react-bootstrap';
  
 
 const AuctionList = () => {
@@ -10,31 +10,25 @@ const AuctionList = () => {
     const { auctions, bids, renderHighBid} = useContext(AuctionContext);
 
     return (
-
-        <div className="auction-List"> 
-  
-            {auctions.map((auction) => (
-            <div><Link to={`/auction/${auction.id}`}>
-            <div className="auctions" key={auction.id}> 
-                <CardGroup>
-                    <Card>
-                        <Card.Img variant="top" src={auction.productImgURL} />
-                        <Card.Body>
-                        <Card.Title>Product name: {auction.productName}</Card.Title>
-                        <Card.Text>
-                            Description: {auction.productInfo}
-                            Starting price: {auction.startPrice}
-                        </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                        {renderHighBid(bids, auction)}
-                        </Card.Footer>
-                    </Card>
-                </CardGroup>
-                    
-            </div></Link> </div>))} 
-         </div>
-
+        <div className="auctionlist">
+        {auctions.map((auction) => (
+        <Row className="auctionCard" xs={12} md={6} lg={4} key={auction.id}>
+        <Card style={{ width: '18rem' }}>
+        <Link to={`/auction/${auction.id}`}>
+            <Card.Header><Card.Title>{auction.productName}</Card.Title></Card.Header>
+            
+                <Card.Img variant="top" src={auction.productImgURL} />
+            <Card.Body>
+                <Card.Text>
+                    {auction.productInfo}
+                    {auction.startPrice}
+                </Card.Text>
+            </Card.Body>
+            {renderHighBid(bids, auction)}
+            </Link>
+        </Card>
+    </Row>))} 
+    </div>
       );
     }
 
